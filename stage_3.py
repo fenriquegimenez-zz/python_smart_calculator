d5a179f370b1e561f5f72571bb821a71e3642c9b
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-09-21 16:10:35 -0400

    added exceptions to the program

--- stage_3.py
+++ stage_3.py
@@ -1,11 +1,16 @@
 while True:
-    n = input()
-    if n == '/help':
-        print('The program calculates the sum and the rest of numbers')
-        continue
-    elif n == '':
-        continue
-    elif n == '/exit':
-        print('Bye!')
-        break
-    print(eval(n))
+    try:
+        n = input()
+        if n == '/help':
+            print('The program calculates the sum and the rest of numbers')
+            continue
+        elif n == '':
+            continue
+        elif n == '/exit':
+            print('Bye!')
+            break
+        elif n[0] == '/' and n != '/exit' and n != '/help':
+            print('Unknown command')
+        print(eval(n))
+    except Exception:
+        print('Invalid expression')
